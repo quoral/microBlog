2156093fd284ab819f4ce46d0c6a1d4a84737606
Karl Johan Krantz <schwomp@gmail.com>
2014-05-08 16:22:28 +0200

    add frontend service for comments

--- client/js/services/commentService.js
+++ client/js/services/commentService.js
@@ -1,3 +1,22 @@
-/**
- * Created by kallekrantz on 08/05/14.
- */
+define([], function(){
+    'use strict';
+    return  ['$http', function($http){
+        return {
+            get: function(postId, commentId){
+                return $http.get('rest/posts/'+postId+'/comments/'+commentId);
+            },
+            getAll: function(postId){
+                return $http.get('rest/posts/'+postId+'/comments');
+            },
+            post: function(postId, postData){
+                return $http.post('rest/posts/'+postId+'/comments',postData);
+            },
+            delete: function(postId, commentId){
+                return $http.delete('rest/posts/'+postId+'/comments/'+commentId);
+            },
+            put: function(id, post){
+                return post;
+            },
+        };
+    }];
+});
