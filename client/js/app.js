b499cd86d52beff1e1dcad1be933fed3dec9c210
Karl Johan Krantz <schwomp@gmail.com>
2014-05-12 11:49:25 +0200

    redirect to / if currentUser changes to something that does not have a authenticated role.

--- client/js/app.js
+++ client/js/app.js
@@ -20,21 +20,44 @@ define([
             $httpProvider.interceptors.push(interceptor);
         });
     }]);
-    app.run(['$rootScope', '$location', 'authService',function ($rootScope, $location, authService ) {
+    app.run(['$rootScope', '$location', '$route', 'authService',function ($rootScope, $location, $route,authService ) {
+        var redirectIfNotAuthenticated = function(roleRequired){
+            if(roleRequired){
+                authService.getUserInfo(true)
+                    .then(function(){
+                        if(!authService.roleIsAuthenticated(authService.currentUser.role, roleRequired)){
+                            $location.path('/');
+                        }
+                    },function(){
+                        // This callback is needed since the callback above is not run on 4xx status codes.
+                        if(!authService.roleIsAuthenticated(authService.currentUser.role, roleRequired)){
+                            $location.path('/');
+                        }
+                    });
+            }
+        };
+        $rootScope.$watch(function () {
+                return authService.currentUser;
+            },
+            function(newVal, oldVal) {
+                if($route.current && $route.current.$$route){
+
+                    var role = $route.current.$$route.requiresUserRole;
+                    console.log(role);
+                    redirectIfNotAuthenticated(role);
+                }
+            },
+            true);
+
+
         $rootScope.$on('$routeChangeStart', function (event, current, previous) {
             if(!current.$$route){
                 return;
             }
             var role = current.$$route.requiresUserRole;
-            if(role){
-                authService.getUserInfo(true)
-                    .then(function(){
-                        if(!authService.roleIsAuthenticated(authService.currentUser.role, role)){
-                            $location.path('/');
-                        }
-                    });
-            }
+            redirectIfNotAuthenticated(role);
         });
+
     }]);
     return app;
 });
