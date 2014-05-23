60063d773268c33f3609faef6175989b437d575b
Karl Johan Krantz <schwomp@gmail.com>
2014-05-23 20:50:44 +0200

    Client now uses a hashmap for posts in order to prepare for live updates being roled out.

--- client/js/services/postService.js
+++ client/js/services/postService.js
@@ -13,15 +13,25 @@ define(['socket.io'], function(io){
         }
 
         var service =  {
-            posts: [],
+            posts: {},
             get: function(id){
-                return $http.get('rest/posts/'+id);
+                return $http.get('rest/posts/'+id)
+                    .then(function(data){
+                        service.posts[data.data.id] = data.data;
+                    });
             },
             getAll: function(forceReload){
                 if(forceReload === true || !getAllPromise){
                     getAllPromise = $http.get('rest/posts')
                         .then(function(data, status){
-                            service.posts = data.data;
+                            data.data.forEach(function(post){
+                                var comments = {};
+                                post.comments.forEach(function(comment){
+                                    comments[comment.id] = comment;
+                                });
+                                post.comments = comments;
+                                service.posts[post.id] = post;
+                            });
                         });
                 }
                 return getAllPromise;
@@ -30,52 +40,49 @@ define(['socket.io'], function(io){
                 return $http.post('rest/posts',postData)
                     .then(function(data, status){
                         data.data.comments = [];
-                        service.posts.push(data.data);
+                        service.posts[data.data.id] = data.data;
                     });
             },
             delete: function(id){
                 return $http.delete('rest/posts/'+id)
                     .then(function(data, status){
-                        var index = findIndexById(service.posts, data.data.id);
-                        service.posts.splice(index, 1);
+                        delete service.posts[id];
                     });
             },
             put: function(id, post) {
                 return $http.put('rest/posts/' + id, post)
                     .then(function (data, status) {
-                        var index = findIndexById(service.posts, data.data.id);
-                        service.posts[index] = data.data;
+                        service.posts[data.data.id] = data.data;
                     });
             },
             comment: {
                 get: function(postId, commentId){
                     return $http.get('rest/posts/'+postId+'/comments/'+commentId)
                         .then(function(data, status){
-                            var postIndex = findIndexById(service.posts, data.data.PostId);
-                            var commentIndex = findIndexById(service.posts[postIndex].comments, data.data.id);
-                            service.posts[postIndex].comments[commentIndex] = data.data;
+                            var comment = data.data;
+                            service.posts[comment.PostId].comments[comment.id] = comment;
                         });
                 },
                 getAll: function(postId){
                     return $http.get('rest/posts/'+postId+'/comments')
                         .then(function(data, status){
-                            var postIndex = findIndexById(service.posts, data.data.PostId);
-                            service.posts[postIndex].comments = data.data;
+                            var comments = {};
+                            data.data.forEach(function(comment){
+                                comments[comment.id] = comment;
+                            });
+                            service.posts[postId].comments = comments;
                         });
                 },
                 post: function(postId, postData){
                     return $http.post('rest/posts/'+postId+'/comments',postData)
                         .then(function(data, status){
-                            var postIndex = findIndexById(service.posts, data.data.PostId);
-                            service.posts[postIndex].comments.push(data.data);
+                            service.posts[data.data.PostId].comments[data.data.id] = data.data;
                         });
                 },
                 delete: function(postId, commentId){
                     return $http.delete('rest/posts/'+postId+'/comments/'+commentId)
                         .then(function(data, status){
-                            var postIndex = findIndexById(service.posts, postId);
-                            var commentIndex = findIndexById(service.posts[postIndex].comments, commentId);
-                            service.posts[postIndex].comments.splice(commentIndex, 1);
+                            delete service.posts[postId].comments[commentId];
                         });
                 },
                 put: function(id, post){
