76cf40a5caaa0b54d05130b8584afa2161b226d4
Karl Johan Krantz <schwomp@gmail.com>
2014-05-27 15:30:53 +0200

    Added modal-directive

--- client/js/directives.js
+++ client/js/directives.js
@@ -1,6 +1,16 @@
-define(['angular', 'services', 'directives/mbPosts', 'directives/mbPost', 'directives/mbComment', 'directives/mbRequiredRole'], function(angular, services, mbPosts, mbPost, mbComment, mbRequiredRole) {
+define(['angular', 'semanticUi','services', 'directives/mbPosts', 'directives/mbPost', 'directives/mbComment', 'directives/mbRequiredRole'], function(angular, semantic, services, mbPosts, mbPost, mbComment, mbRequiredRole) {
     'use strict';
 
+    //Stolen from angular
+    function toBoolean(value) {
+        if (value && value.length !== 0) {
+            var v = ('' + value).toLowerCase();
+            value = !(v === 'f' || v === '0' || v === 'false' || v === 'no' || v === 'n' || v === '[]');
+        } else {
+            value = false;
+        }
+        return value;
+    }
     /* Directives */
     angular.module('microBlog.directives', ['microBlog.services'])
         .directive('mbLoginButtons', [function() {
@@ -26,6 +36,47 @@ define(['angular', 'services', 'directives/mbPosts', 'directives/mbPost', 'direc
                         element.addClass('active');
                     }
                 };
+        }])
+        .directive('mbModal', ['$parse', function($parse){
+            return {
+                link: function (scope, element, attr) {
+                    function applyFunction(fn){
+                        return function(){
+                            scope.$apply(function() {
+                                fn(scope);
+                            });
+                        };
+                    }
+                    var onHidden, onDeny, onApprove;
+                    onDeny = onHidden = onApprove = function(){};
+                    if(attr.onHidden){
+                        onHidden = $parse(attr.onHidden);
+                    }
+                    if(attr.onApprove){
+                        onApprove = $parse(attr.onApprove);
+                    }
+                    if(attr.onDeny){
+                        onDeny = $parse(attr.onDeny);
+                    }
+
+                    function showModal(value) {
+                        if(toBoolean(value)){
+                            element
+                                .modal('setting', {
+                                    onDeny: applyFunction(onDeny),
+                                    onApprove: applyFunction(onApprove),
+                                    onHidden: applyFunction(onHidden)
+                                })
+                                .modal('show')
+                            ;
+                        }
+                    }
+
+                    attr.$observe('mbModal', function() {
+                        showModal(attr.mbModal);
+                    });
+                }
+            };
         }])
         .directive('mbRequiredRole', mbRequiredRole)
         .directive('mbPosts', mbPosts)
